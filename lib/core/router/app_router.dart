bc602dcd484e8821be5b1126e35e1cc787d98e6f
CleisonFreitas <cleison51@hotmail.com>
2025-05-19 23:57:17 -0300

    creating the initial login screen

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,4 +1,5 @@
 import 'package:go_router/go_router.dart';
+import 'package:local_services/features/auth/sigin/home_screen.dart';
 import 'package:local_services/features/auth/sigin/signin.dart';
 import 'package:local_services/features/auth/signup/signup.dart';
 import 'package:local_services/features/auth/splash/splash_screen.dart';
@@ -7,6 +8,7 @@ final routerConfig = GoRouter(
   initialLocation: '/',
   routes: [
     GoRoute(path: '/', builder: (context, state) => const SplashScreen()),
+    GoRoute(path: '/home', builder: (context, state) => const HomeScreen()),
     GoRoute(path: '/login', builder: (context, state) => const SignIn()),
     GoRoute(path: '/register', builder: (context, state) => const SignUp()),
   ],
